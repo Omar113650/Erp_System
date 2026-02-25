aefe4fe7048b74b586138a03fa75ca3d0e057573
Omar113650 <omarelhelaly520@gmail.com>
2026-02-25 04:00:40 +0200

    fix missing streamifier dependency

--- erp/src/main.ts
+++ erp/src/main.ts
@@ -25,36 +25,101 @@
 
 
 
-import 'tsconfig-paths/register';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { VersioningType } from '@nestjs/common';
-import * as dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-dotenv.config();
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+// import 'tsconfig-paths/register';
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { VersioningType } from '@nestjs/common';
+// import * as dotenv from 'dotenv';
+// import cookieParser from 'cookie-parser';
+// dotenv.config();
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
 
   
+
+//   // Global Prefix
+//   app.setGlobalPrefix('api');
+
+
+
+//   app.use(cookieParser());
+
+//   app.enableVersioning({
+//     type: VersioningType.URI, // النوع
+//     defaultVersion: '1', // النسخة الافتراضية لو مش محددة
+//   });
+
+//   // PORT ديناميكي مع Vercel
+//   const port = process.env.PORT || 8000;
+//   await app.listen(port);
+//   console.log(`Server running on http://localhost:${port}`);
+// }
+// bootstrap();
+
+
+
+
+
+
+
+
+import 'tsconfig-paths/register';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { VersioningType } from '@nestjs/common';
+import { createServer, Server } from 'http';
+import cookieParser from 'cookie-parser';
+
+let server: Server;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
 
   // Global Prefix
   app.setGlobalPrefix('api');
 
-
-
   app.use(cookieParser());
 
   app.enableVersioning({
-    type: VersioningType.URI, // النوع
-    defaultVersion: '1', // النسخة الافتراضية لو مش محددة
+    type: VersioningType.URI,
+    defaultVersion: '1',
   });
 
-  // PORT ديناميكي مع Vercel
-  const port = process.env.PORT || 8000;
-  await app.listen(port);
-  console.log(`Server running on http://localhost:${port}`);
+  await app.init();
+
+  server = createServer(app.getHttpAdapter().getInstance());
 }
+
 bootstrap();
+
+// هنا بنصدّر default function
+export default async (req, res) => {
+  if (!server) {
+    res.statusCode = 503;
+    return res.end('Server not ready');
+  }
+
+  server.emit('request', req, res);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
